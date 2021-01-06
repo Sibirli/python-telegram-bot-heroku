208ae40aaee70e8fa0f79ef5c15f439ffc0ae18e
Sibirli <royalnagiyev@gmail.com>
2021-01-06 17:43:35 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,13 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
+import sys
+import re
+from time import time, localtime, strftime, sleep
+import telebot
+import requests
+from Levenshtein import distance as levenshtein_distance
+import config
 PORT = int(os.environ.get('PORT', 5000))
 
 # Enable logging
@@ -23,69 +30,73 @@ def help(update, context):
     
     
 
+dictmap = {}
 
-runic_dict = {
-    "ing": "\u16DD",
-    "ae": "\u16AB",
-    "th": "\u16A6",
-    "ea": "\u16E0",
-    "ia": "\u16e1",
-    "io": "\u16e1",
-    "oe": "\u16DF",
-    "ee": "\u16DF",
-    "gh": "\u16B8",
-    "kh": "\u16E4",
-    "a": "\u16AA",
-    "b": "\u16D2",
-    "c": "\u16B3",
-    "d": "\u16DE",
-    "e": "\u16D6",
-    "f": "\u16A0",
-    "g": "\u16B7",
-    "h": "\u16BB",
-    "i": "\u16C1",
-    "j": "\u16C4",
-    "k": "\u16e3",
-    "l": "\u16DA",
-    "m": "\u16D7",
-    "n": "\u16BE",
-    "o": "\u16A9",
-    "p": "\u16C8",
-    "q": "\u16E2",
-    "r": "\u16B1",
-    "s": "\u16CB",
-    "t": "\u16CF",
-    "u": "\u16A2",
-    #        "v":"\u16A2",
-    "v": "\u16A1",  # medieval version
-    "w": "\u16B9",
-    "x": "\u16C9",
-    "y": "\u16A3",
-    "z": "\u16CE",
-    " ": "\u16eb",
-    ",\u16eb": " \u16ec ",
-    ";\u16eb": " \u16ec ",
-    ":\u16eb": " \u16ec ",
-    ".\u16eb": " \u16ed ",
-    "?\u16eb": " \u16ed ",
-    "!\u16eb": " \u16ed ",
-    "'\u16eb": " \u16eb ",
-    "'": " \u16eb ",
-    ",": " \u16ec ",
-    ";": " \u16ec ",
-    ":": " \u16ec ",
-    ".": " \u16ed ",
-    "?": " \u16ed ",
-    "!": " \u16ed ",
-    '"': "",
-}
+def load_dict(code,fn):
+  dictmap[code] = [] 
+  with open(fn,"r") as f:
+    for l in f:
+      if l.startswith('#') or l.startswith(' '):
+        continue
+      m = re.search(r'^(\S+)\s(\S+)', l)
+      if not m:
+        print ("ERROR: [%s]" % l)
+        continue
+      m1 = m.group(1)
+      m2 = m.group(2)
+      dictmap[code].append((m1, m2))
 
+def translate(code, text):
+  for k,v in dictmap[code]:
+    text = re.sub(k, v, text)
+  return text
 
-def runify(text):
-    text = text.lower()
-    for k, v in runic_dict.items():
-        text = text.replace(k, v)
-    return text
+load_dict("glag2cyrl","glag2cyrl.tab")
+load_dict("tfng2cyrl","tfng2cyrl.tab")
+load_dict("cyrl2glag","cyrl2glag.tab")
+load_dict("cyrl2tfng","cyrl2tfng.tab")
+
+# команда /rules для @mikitkinabeseda
+@bot.message_handler(commands=["rules"])
+def rules(message):
+  if message.chat.type in ['group','supergroup'] and message.chat.id == -1001199017575:
+    with open("rules.md", "r") as f:
+      rules = f.read()
+    bot.send_message(message.chat.id, rules, parse_mode="Markdown")
+
+@bot.message_handler(content_types=['text'])
+def translate_message(message):
+  msg = message.text
+  print ("%s|%s <%s %s> %s" % (str(message.chat.id), strftime("%Y-%m-%d %H:%M:%S", localtime(message.date)), message.from_user.first_name, message.from_user.last_name, msg))
+  if time() > message.date+config.max_timediff:
+    print (" message time too old :(")
+    return
+  for code in config.default_tabs:
+    msgtr = translate(code, msg)
+    dist = levenshtein_distance(msg, msgtr)
+    ratio = dist/len(msg)
+    if ratio > config.min_levenshtein_ratio:
+      print (" code=%s ratio=%lf => %s" % (code, ratio, msgtr))
+      try:
+        if config.test_mode:
+          msgtr = "[TEST MODE] "+msgtr
+        bot.send_message(message.chat.id, msgtr, reply_to_message_id=message.message_id)
+      except telebot.apihelper.ApiException:
+        print (" Exception occured!")
+      return
+
+# inline-режим, у боевого бота выключен, так как нельзя запретить использовать его в конкретном чате
+@bot.inline_handler(lambda query: len(query.query) > 0)
+def query_text(inline_query):
+  print (inline_query)
+  msgtr_glag = translate('cyrl2glag', inline_query.query)
+  msgtr_tfng = translate('cyrl2tfng', inline_query.query)
+  if config.test_mode:
+    msgtr_glag = "[TEST MODE] "+msgtr_glag
+    msgtr_tfng = "[TEST MODE] "+msgtr_tfng
+  r_glag = telebot.types.InlineQueryResultArticle('GLAG', f'{inline_query.query} -> {msgtr_glag}', telebot.types.InputTextMessageContent(msgtr_glag))
+  r_tfng = telebot.types.InlineQueryResultArticle('TFNG', f'{inline_query.query} -> {msgtr_tfng}', telebot.types.InputTextMessageContent(msgtr_tfng))
+  bot.answer_inline_query(inline_query.id, [r_glag, r_tfng])
     
     
     
